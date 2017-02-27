7498c6ce82c391cf6545b750d53131b951b1c4c7
Brandon Leichty <brandonl@me.com>
2017-02-27 13:10:50 -0600

    added function that gets five most recent tweets and user profile image

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const pug = require('pug');
 const twit = require('twit');
-const config = require('./config');
+const os = require('os');
+const config = require('../config.js');
 
 const port = 3030;
 
 
 const app = express();
+var T = new twit(config);
+
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -16,6 +19,19 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('main.pug');
+});
+
+//get five most recent tweets
+T.get('statuses/user_timeline', {count: 5}, (err, data) => {
+  data.map(element => {
+    console.log(`${element.text} ${os.EOL}`);
+  });
+});
+
+
+//get user profile information
+T.get('account/verify_credentials', (error, data) => {
+  console.log(data.profile_image_url);
 });
 
 
