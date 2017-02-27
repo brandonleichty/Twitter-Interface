d542dd695bc57146f299726cc50ae3c2d885fc50
Brandon Leichty <brandonl@me.com>
2017-02-27 15:34:42 -0600

    added GET request that get the required Twitter information and then writes it to the console.

--- src/app.js
+++ src/app.js
@@ -17,24 +17,52 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.get('/', function(req, res){
-  res.render('main.pug');
+app.get('/', function(req, res) {
+    res.render('main.pug');
 });
 
 //get five most recent tweets
-T.get('statuses/user_timeline', {count: 5}, (err, data) => {
-  data.map(element => {
-    console.log(`${element.text} ${os.EOL}`);
-  });
+T.get('statuses/user_timeline', {
+    count: 5
+}, (error, data) => {
+    console.log(`${os.EOL}YOUR MOST RECENT TWEETS: ${os.EOL}`);
+    data.map(tweet => {
+        console.log(`${tweet.text}`);
+        console.log(`Originally tweeted on: ${tweet.created_at}`);
+        console.log(`Number of retweets: ${tweet.retweet_count}`);
+        console.log(`Favorited: ${tweet.favorite_count} times${os.EOL}`);
+    });
 });
 
 
 //get user profile information
 T.get('account/verify_credentials', (error, data) => {
-  console.log(data.profile_image_url);
+    console.log(`${os.EOL}YOUR PROFILE INFORMATION IS: ${os.EOL}`);
+    console.log(data.name);
+    console.log(data.screen_name);
+    console.log(data.profile_image_url);
+});
+
+
+//get five most recent frinds
+T.get('friends/list', {
+    count: 5
+}, (error, data) => {
+    console.log(`${os.EOL}YOUR MOST RECENT FRIENDS ARE: ${os.EOL}`);
+    data.users.forEach(user => {
+        console.log(`${user.screen_name} ${os.EOL}`);
+    });
+});
+
+
+T.get('direct_messages', (error, data) => {
+  data.forEach(message => {
+    console.log(message.text);
+  });
+
 });
 
 
 app.listen(port, () => {
-  console.log('The server is now running on port 3030. Press CTRL+C to quit.');
+    console.log('The server is now running on port 3030. Press CTRL+C to quit.');
 });
