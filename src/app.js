520cdeb74fcc81eb36a7b73f7ff884cd44b29d31
Brandon Leichty <brandonl@me.com>
2017-02-27 12:13:42 -0600

    added Pug views/partials

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
 const express = require('express')
+const pug = require('pug');
 const twit = require('twit');
+
+const port = 3030;
+
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', function(req, res){
+  res.render('main.pug');
+});
+
+
+app.listen(port, () => {
+  console.log('The server is now running on port 3030. Press CTRL+C to quit.');
+});
